445df5ec2e6223ea08d6562db582d6ddfc6cb0be
gophemt <gophemt@hotmail.com>
2020-05-10 00:18:38 +0100

    Adds Drop Database

---
+++ dropd.go
@@ -0,0 +1,16 @@
+package mgorm
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// DropDatabase drops database
+func (d *Db) DropDatabase(mgo *mongo.Client) error {
+	err := mgo.Database(d.DatabaseName).Drop(context.TODO())
+	if err != nil {
+		return err
+	}
+	return nil
+}
